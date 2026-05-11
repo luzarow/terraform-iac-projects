5dbd6174cb39e0a0e1290e1f0ff06bc0915b4017
mateen <mateen.devsec@gmail.com>
2026-05-11 14:00:31 -0400

    AWS EC2 and Security Group with best practices

--- 01-AWS-EC2-Provisioning-With-Terraform/ec2.tf
+++ 01-AWS-EC2-Provisioning-With-Terraform/ec2.tf
@@ -18,11 +18,11 @@ resource "aws_instance" "terraform_instance" {
               systemctl start httpd
               systemctl enable httpd
 
-              echo "<h1>Hello from Terraform Lab!</h1>" > /var/www/html/index.html
+              echo "<h1>Hello from Mateen's Infra Provisioning Terraform!</h1>" > /var/www/html/index.html
               EOF
 
   tags = {
     Name        = "terraform-lab-instance"
     Environment = var.environment
   }
-}
+} 
